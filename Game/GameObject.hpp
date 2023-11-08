27232db7029ff26496102f1942a168b0d8236c51
Shekhov Ismoil <veyxov@gmail.com>
2023-11-08 09:25:08 +0500

    refactor: added game object class

---
+++ Game/GameObject.hpp
@@ -0,0 +1,26 @@
+#include "Game.hpp"
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_rect.h>
+#include <sys/types.h>
+class GameObject {
+public:
+  GameObject(cwt::game &game, const char *texture_path, int x, int y, bool has_keys = false) {
+    cwt::entity ent = cwt::create_entity();
+
+    auto& registry = game.get_registry();
+    registry.sprites[ent] = cwt::sprite_component{
+        SDL_Rect{0, 0, width, height}, SDL_Rect{x, y, width, height},
+        IMG_LoadTexture(game.get_renderer(), texture_path)};
+
+    registry.transforms[ent] =
+        cwt::transform_component{(float)x, (float)y, 0, 0};
+
+    if (has_keys) {
+        registry.keys[ent] = cwt::keyinputs_component{};
+    }
+  }
+
+private:
+  int height = 100;
+  int width = 100;
+};
--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include "Game/Game.hpp"
+#include "Game/GameObject.hpp"
 
 int main(int argc, char *argv[]) {
   cwt::game game(800, 600);
 
-  const char *box_path = "assets/box.png";
-  const char *player_path = "assets/player.png";
-
-  cwt::entity box = cwt::create_entity();
-  game.get_registry().sprites[box] =
-      cwt::sprite_component{SDL_Rect{0, 0, 300, 230}, SDL_Rect{10, 10, 100, 73},
-                            IMG_LoadTexture(game.get_renderer(), box_path)};
-  game.get_registry().transforms[box] = cwt::transform_component{10, 10, 0, 0};
-  // No need for box movement
-  //game.get_registry().keys[box] = cwt::keyinputs_component{};
-
-  cwt::entity player = cwt::create_entity();
-  game.get_registry().sprites[player] =
-      cwt::sprite_component{SDL_Rect{0, 0, 222, 230}, SDL_Rect{10, 10, 100, 73},
-                            IMG_LoadTexture(game.get_renderer(), player_path)};
-  game.get_registry().transforms[player] =
-      cwt::transform_component{100, 100, 0, 0};
-  game.get_registry().keys[player] = cwt::keyinputs_component{};
+  GameObject box(game, "assets/box.png", 10, 10);
+  GameObject player(game, "assets/player.png", 100, 100, true);
 
   while (game.is_running()) {
     game.read_input();
